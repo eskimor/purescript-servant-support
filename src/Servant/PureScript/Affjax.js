a6adf372cf9eb7f447085cff86afec9e81cd638d
Robert Klotzner <robert.klotzner@gmx.at>
2016-06-30 13:30:11 +0200

    Also use responseText in browser

--- src/Servant/PureScript/Affjax.js
+++ src/Servant/PureScript/Affjax.js
@@ -41,7 +41,7 @@ exports._ajax = function (mkHeader, options, canceler, errback, callback) {
     };
 
     platformSpecific.getResponse = function (xhr) {
-      return xhr.response;
+      return xhr.responseText;
     };
   }
 
